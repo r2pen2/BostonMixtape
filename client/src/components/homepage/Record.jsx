38a919715a17a7fe3def558dbe656ebb085f5aa4
r2pen2 <joedobbelaar@gmail.com>
2024-06-15 01:15:16 -0400

    recordtray

--- client/src/components/homepage/Record.jsx
+++ client/src/components/homepage/Record.jsx
@@ -7,6 +7,8 @@ import recordHotPink from "../../assets/images/homepage/records/recordHotPink.pn
 import recordIceBlue from "../../assets/images/homepage/records/recordIceBlue.png"
 import recordPurple from "../../assets/images/homepage/records/recordPurple.png"
 import recordRed from "../../assets/images/homepage/records/recordRed.png"
+import recordSteel from "../../assets/images/homepage/records/recordSteel.png"
+import { useState } from "react"
 
 export const RecordColor = {
   Gold: "gold",
@@ -14,10 +16,29 @@ export const RecordColor = {
   HotPink: "hot-pink",
   IceBlue: "ice-blue",
   Purple: "purple",
-  Red: "red"
+  Red: "red",
+  Steel: "steel",
 };
 
-export const Record = ({color, active, glareRotation}) => {
+export const RecordTray = ({mousePosition}) => {
+  
+  const [activeRecord, setActiveRecord] = useState(0);
+
+  return (
+    <div className="record-tray">
+      <Record color={RecordColor.Gold}    active={activeRecord === 0} mousePosition={mousePosition} />
+      <Record color={RecordColor.Green}   active={activeRecord === 1} mousePosition={mousePosition} />
+      <Record color={RecordColor.HotPink} active={activeRecord === 2} mousePosition={mousePosition} />
+      <Record color={RecordColor.IceBlue} active={activeRecord === 3} mousePosition={mousePosition} />
+      <Record color={RecordColor.Purple}  active={activeRecord === 4} mousePosition={mousePosition} />
+      <Record color={RecordColor.Red}     active={activeRecord === 5} mousePosition={mousePosition} />
+      <Record color={RecordColor.Steel}   active={activeRecord === 6} mousePosition={mousePosition} />
+    </div>
+  )
+
+}
+
+export const Record = ({color, active, mousePosition}) => {
 
   function getSrc() {
     switch(color) {
@@ -33,10 +54,14 @@ export const Record = ({color, active, glareRotation}) => {
         return recordPurple;
       case RecordColor.Red:
         return recordRed;
+      case RecordColor.Steel:
+        return recordSteel;
       default:
         return recordGold;
     }
   }
+
+  const glareRotation = Math.atan2(mousePosition.y, mousePosition.x) * 180 / Math.PI + 90;
 
   return (
     <div className={"record " + (active ? "active" : "")}>
--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,29 +1,28 @@
 import React, { useState } from 'react'
 
 import "../assets/style/home.css"
-import { Record, RecordColor } from '../components/homepage/Record'
+import { Record, RecordColor, RecordTray } from '../components/homepage/Record'
 
 export default function Home() {
   
-  const [glareRotation, setGlareRotation] = useState(0)
+  const [mousePosition, setMousePosition] = useState({x: 0, y: 0});
 
   const handleMouseMove = (e) => {
     const rect = e.target.getBoundingClientRect();
     const x = e.clientX - rect.left - rect.width / 2;
     const y = e.clientY - rect.top - rect.height / 2;
-    const angle = Math.atan2(y, x) * 180 / Math.PI + 90;
-    setGlareRotation(angle);
+    setMousePosition({x, y});
+    // const angle = Math.atan2(y, x) * 180 / Math.PI + 90;
   };
-  
   
   return (
     <div className="homepage-container">
-      <div className="home-splash-container" onMouseMove={handleMouseMove} style={{"--record-glare-rotation": glareRotation}}>
+      <div className="home-splash-container" onMouseMove={handleMouseMove}>
         <hgroup>
           <h1 className="anton-regular header-line-1">BERACH</h1>
           <h1 className="anton-regular header-line-2">Orchestra</h1>
         </hgroup>
-        <Record active color={RecordColor.Red} glareRotation={glareRotation}/>
+        <RecordTray mousePosition={mousePosition} />
       </div>
     </div>
   )
